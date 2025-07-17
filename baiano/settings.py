b68c519a49d714c14d23d6f2e33092c1d40965da
Rafael Ferreira <rafaelf_s@hotmail.com>
2025-07-17 20:42:21 +0100

    Connect to the database

--- baiano/settings.py
+++ baiano/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,9 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zvgr7qm808ocs2#_5xqlh^yw^@y%bpf8#pc6@5m5ey%-ikii_g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.herokuapp.com',
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -37,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restaurant',
 ]
 
 MIDDLEWARE = [
@@ -73,13 +82,16 @@ WSGI_APPLICATION = 'baiano.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- readme.md
+++ readme.md
@@ -1,1 +1,29 @@
+# Baiano Restaurant
+
+## Deployment
+
+Here is a step by step how I deployed in Heroku
+
+### GitHub
+
+1. Create a new repository in GitHub
+2. Clone the repository
+3. Install Django, and create the project
+4. Create a simple view for the home page
+5. Install gunicorn
+6. Add Heroku and Localhost to Allowed Hosts
+7. Craete the database
+8. Create env.py file, and add the database url
+9. Install psycopg2 and dj-database-url
+10. After connecting to the database, migrate the models
+
+### Heroku
+
+1. Login to Heroku
+2. Click on 'New'
+3. From the 'New' dropdown, click on 'Create new app'
+4. Give it a name
+5. Choose a location
+6. In your app, go to settings, and add the database url
+7. On deployment method, link to the GitHub repository
 
