711cb1a94714550088e03633bc6ee30eec6290f4
Rafael Ferreira <rafaelf_s@hotmail.com>
2025-08-05 22:45:21 +0100

    Add X_FRAME_OPTIONS

--- baiano/settings.py
+++ baiano/settings.py
@@ -164,3 +164,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+X_FRAME_OPTIONS = os.environ.get('X_FRAME_OPTIONS', 'DENY')
